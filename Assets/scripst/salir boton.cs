28215115b7df3621e8c138e5a5d3d6c5c25b8af8
Alexator <alexatorlulu@gmail.com>
2024-09-20 18:00:43 +0200

    comentando lo que falto

--- Assets/scripst/salir boton.cs
+++ Assets/scripst/salir boton.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class salirboton : MonoBehaviour
-{
-
-  
+{//boton salir dle juego
 public void Salirse(){
      Application.Quit();
 }
